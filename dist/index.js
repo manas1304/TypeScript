6acb219e1c2c9111aa40665d62fc92b9d4d2e279
Manas Shukla <manasshukla01304@gmail.com>
2026-02-09 12:51:43 +0530

    TypeScript Project Setup and First Program

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function greet(person) {
+    return `Hello ${person}, This is my first TypeScript programme`;
+}
+const userName = "Manas";
+console.log(greet(userName));
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+function greet(person: string): string{
+    return `Hello ${person}, This is my first TypeScript programme`
+}
+
+const userName: string = "Manas";
+console.log(greet(userName))
